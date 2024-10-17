eb6d4e02f581be92712712a96e695abd7a7d0206
benoitliard <benoit.liard@gmail.com>
2024-10-17 16:42:21 -0400

    fix stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,3 +2,6 @@ import '../src/css/main.css';
 import '../src/css/custom.css';
 
 // ... rest of your _app.tsx file
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />
+  }
