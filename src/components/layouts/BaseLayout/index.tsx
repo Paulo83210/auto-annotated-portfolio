3b013e718b01705764a56c3d84ba41198f61d926
Benoit Liard <benoit.liard@gmail.com>
2024-10-17 16:13:10 -0400

    update fonts

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -26,6 +26,10 @@ const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
                 <div className="sb-base sb-default-base-layout relative">
                     <Head>
                         <title>{title}</title>
+                        <link rel="preconnect" href="https://fonts.googleapis.com" />
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                        <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
                         {metaDescription && <meta name="description" content={metaDescription} />}
                         {metaTags.map((metaTag) => {
                             if (metaTag.format === 'property') {
