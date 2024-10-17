d757b8ed5242e48b691135886676a2a48309f31a
benoitliard <benoit.liard@gmail.com>
2024-10-17 16:46:12 -0400

    quick fix2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from 'next/app'
 import '../src/css/main.css';
 import '../src/css/custom.css';
 
 // ... rest of your _app.tsx file
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
-  }
+}
